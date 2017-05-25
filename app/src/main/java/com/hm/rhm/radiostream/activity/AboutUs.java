85ca439355d8a3449a3d957414b9e815c52ec562
soklundy <bonglundy208@gmail.com>
2017-05-25 22:00:39 +0700

    fix25/05/17_10:00

--- app/src/main/java/com/hm/rhm/radiostream/activity/AboutUs.java
+++ app/src/main/java/com/hm/rhm/radiostream/activity/AboutUs.java
@@ -1,16 +1,28 @@
 package com.hm.rhm.radiostream.activity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import com.hm.rhm.radiostream.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class AboutUs extends AppCompatActivity {
+
+    @BindView(R.id.webview)
+    WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        ButterKnife.bind(this);
+        mWebView.loadUrl("file:///android_asset/about_us.html");
+        mWebView.setBackgroundColor(Color.TRANSPARENT);
+        mWebView.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
     }
 }
