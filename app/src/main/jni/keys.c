b11e0fb32d31cf534e36cd5a7771e1e52deab92f
soklundy <bonglundy208@gmail.com>
2018-11-05 09:39:34 +0700

    1.9

--- app/src/main/jni/keys.c
+++ app/src/main/jni/keys.c
@@ -8,24 +8,24 @@ JNIEXPORT jstring JNICALL
 Java_com_hm_rhm_radiostream_activity_MainActivity_getHmTv(JNIEnv *env, jobject instance) {
 
     return (*env)->NewStringUTF(env,
-                                "http://111.92.240.134:1934/hm_hdtv/smil:HMHDTV.smil/playlist.m3u8");
+                                "http://malisresidencess.com:1934/redirect/hm_hdtv/smil:HMHDTV.smil?scheme=m3u8");
 }
 
 JNIEXPORT jstring JNICALL
 Java_com_hm_rhm_radiostream_activity_MainActivity_getRhmTv(JNIEnv *env, jobject instance) {
 
     return (*env)->NewStringUTF(env,
-                                "http://111.92.240.134:1934/rhm_hdtv/smil:RHMHDTV.smil/playlist.m3u8");
+                                "http://malisresidencess.com:1934/redirect/rhm_hdtv/smil:RHMHDTV.smil?scheme=m3u8");
 }
 
 JNIEXPORT jstring JNICALL
 Java_com_hm_rhm_radiostream_activity_MainActivity_getRhmRadio(JNIEnv *env, jobject instance) {
 
-    return (*env)->NewStringUTF(env, "http://111.92.240.134:90/broadwavehigh.mp3");
+    return (*env)->NewStringUTF(env, "http://malisresidencess.com:90/broadwavehigh.mp3?src=1");
 }
 
 JNIEXPORT jstring JNICALL
 Java_com_hm_rhm_radiostream_activity_MainActivity_getHmRadio(JNIEnv *env, jobject instance) {
 
-    return (*env)->NewStringUTF(env, "http://111.92.240.134:89/broadwavehigh.mp3");
+    return (*env)->NewStringUTF(env, "http://malisresidencess.com:89/broadwavehigh.mp3?src=1");
 }
